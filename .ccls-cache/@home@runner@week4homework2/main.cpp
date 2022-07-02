593b3527dbafcfc18d8c2789c3f42ec69403477a
Yisi Liu <85250232+yisliu@users.noreply.github.com>
2022-07-02 00:42:43 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@week4homework2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str1;
+  cin>>str1;
+  int count=0;
+  int counter=0;
+  for(int i = 0; i<str1.length(); i++){
+    if(str1[i]=='A'){
+      count++;
+    }
+    else if(str1[i]=='L'){
+      counter++;
+    }
+    else{
+      continue;
+    }
+  }
+  if(count>2){
+    cout<<"false";
+  }
+  else if(counter>3){
+    cout<<"false";
+  }
+  else{
+    cout<<"true";
+  }
+}
